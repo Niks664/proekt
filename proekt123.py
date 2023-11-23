228b8651e0e6f372f5d183a4625144b061293f7b
Niks664 <77021844+Niks664@users.noreply.github.com>
2023-11-23 20:09:20 +0300

    Add files via upload

---
+++ proekt123.py
@@ -0,0 +1,87 @@
+from flask import Flask,request, Response, render_template
+import uuid
+import time
+from flask_cors import CORS
+import json
+
+app = Flask(__name__,static_url_path='/static')
+
+CORS(app)
+
+app = Flask(__name__)
+
+users = {}
+msg = []
+name = ""
+text = ""
+
+
+@app.route("/")
+def main():
+    return ("/auth")
+
+
+@app.route('/auth')
+def auth():
+    name = request.args.get('name')
+    if name not in users:
+        token = str(uuid.uuid4())
+        users[name] = token
+        return Response(token, status = 200, mimetype = "text/plain")
+    elif name == None:
+        return Response("Вы не указали имя!",status = 400, mimetype = "text/plain")
+    else:
+        return Response("Такой пользователь уже есть",status = 403, mimetype = "text/plain")
+
+
+@app.route('/users')
+def usersss():
+    return (f'{users}')
+
+@app.route('/send')
+def send():
+    text = request.args.get('text')
+    token = request.args.get('token')
+    if text == None:
+        return Response('Текст не указан',status = 400)
+    if token == None:
+        return Response("Токен не указан",status = 400)
+    name = None
+    for key,value in users.items():
+        if value == token:
+            name = key
+    if name == None:
+        return Response("Имя пустое",status = 403, mimetype = "text/plain")
+    
+    timestamp = time.time()
+    msg.append({'name': name,
+                'text': text,
+                'time': timestamp})
+    return Response("Сообщение отправлено", status = 200, mimetype = "text/plain")
+    
+    
+    
+@app.route("/logout")
+def logout():
+    token = request.args.get('token')
+    if token == None:
+        return Response("Токен не указан",status = 400, mimetype = "text/plain")
+    for key,value in users.items():
+        if value == token:
+            del users[key]
+            return Response("Вы успешно вышли из системы", status = 200, mimetype = "text/plain")
+    return Response("Токен не найден",status = 403, mimetype = "text/plain")
+        
+
+
+@app.route('/getall')
+def getall():
+    token = request.args.get('token')
+    if token == None or token == "" or token not in users.values():
+        return Response("Токен не найден",status = 403)
+    
+    return Response(json.dumps(msg), status = 200)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
